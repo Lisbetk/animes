2b690e615098b7b28c13d825c847aa82a2f8dc5e
lis <lisbetfelix62@mail.com>
2016-03-10 09:04:09 -0500

    trabajo pagina

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Malware</title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+
+
+	<div class="container-fluid">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Otaku</a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Animes Accion</a></li>
+						<li><a href="#">Animes Romanticos</a></li>
+					</ul>
+					<form class="navbar-form navbar-right" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="escribe nombre Anime">
+						</div>
+						<button type="submit" class="btn btn-default">Buscar</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Unirme al Grupo</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Descargar <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+
+		<!--slider-->
+		<div id="carousel-id" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-id" data-slide-to="0" class=""></li>
+                    <li data-target="#carousel-id" data-slide-to="1" class=""></li>  
+                    <li data-target="#carousel-id" data-slide-to="2" class=""></li>  
+                    <li data-target="#carousel-id" data-slide-to="3" class=""></li>                        
+                    <li data-target="#carousel-id" data-slide-to="4" class="active"></li>
+                    
+                </ol>
+            
+                <div class="carousel-inner">
+                    <div class="item">
+                        <img data-src="public/imagenes/1.jpg" alt="First slide" src="public/imagenes/1.jpg">
+
+                    </div>
+                    <div class="item">
+                        <img data-src="public/imagenes/2.jpg" alt="Second slide" src="public/imagenes/2.jpg">
+
+                    </div>
+                    <div class="item active">
+                        <img data-src="public/imagenes/3.jpg" alt="Third slide" src="public/imagenes/3.jpg">
+
+                    </div>
+                    <div class="item">
+                        <img data-src="public/imagenes/4.jpg" alt="Second slide" src="public/imagenes/4.jpg">
+
+                    </div>
+                    <div class="item">
+                        <img data-src="public/imagenes/5.jpg" alt="Second slide" src="public/imagenes/5.jpg">
+
+                    </div>
+                   
+
+                </div>
+                
+                <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                              
+
+        </div>
+           
+      
+	<!-- Latest compiled and minified JavaScript -->
+
+	</div>
+
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+
+</body>
+</html>
